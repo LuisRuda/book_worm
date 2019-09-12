34fd7aa1c280689e2c33b27a7d9da5cb12303ff2
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-09-12 20:32:27 -0300

    Fix: styles in WelcomeScreen

--- src/screens/AppSwitchNavigator/WelcomeScreen.js
+++ src/screens/AppSwitchNavigator/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import Icon from 'react-native-ionicons';
 
 import colors from '../../assets/colors';
@@ -8,44 +8,69 @@ import CustomActionButton from '../../components/CustomActionButton';
 export default class WelcomeScreen extends Component {
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: colors.bgMain}}>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={styles.container}>
+        <View style={styles.containerLogo}>
           <Icon
             ios="ios-bookmarks"
             android="md-bookmarks"
             size={150}
             color={colors.logoColor}
           />
-          <Text style={{fontSize: 50, fontWeight: '100', color: '#fff'}}>
-            Book Worm
-          </Text>
+          <Text style={styles.title}>Book Worm</Text>
         </View>
-        <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={styles.containerButtons}>
           <CustomActionButton
-            style={{
-              width: 200,
-              backgroundColor: 'transparent',
-              borderWidth: 0.5,
-              borderColor: colors.bgPrimary,
-              marginBottom: 10,
-            }}
+            style={styles.loginButton}
             title="Login in"
             onPress={() => this.props.navigation.navigate('HomeScreen')}>
-            <Text style={{fontWeight: '100', color: '#fff'}}>Login In</Text>
+            <Text style={styles.text}>Login In</Text>
           </CustomActionButton>
           <CustomActionButton
-            style={{
-              width: 200,
-              backgroundColor: 'transparent',
-              borderWidth: 0.5,
-              borderColor: colors.bgError,
-            }}
+            style={styles.logoutButton}
             title="Sign up"
             onPress={() => this.props.navigation.navigate('SignUpScreen')}>
-            <Text style={{fontWeight: '100', color: '#fff'}}>Sign Up</Text>
+            <Text style={styles.text}>Sign Up</Text>
           </CustomActionButton>
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.bgMain,
+  },
+  containerLogo: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: '100',
+    color: '#fff',
+  },
+  containerButtons: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  loginButton: {
+    width: 200,
+    backgroundColor: 'transparent',
+    borderWidth: 0.5,
+    borderColor: colors.bgPrimary,
+    marginBottom: 10,
+  },
+  logoutButton: {
+    width: 200,
+    backgroundColor: 'transparent',
+    borderWidth: 0.5,
+    borderColor: colors.bgError,
+  },
+  text: {
+    fontWeight: '100',
+    color: '#fff',
+  },
+});
